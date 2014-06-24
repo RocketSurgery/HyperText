a08e9b37049cc9bb808c11c062ec94ff94619b34
David LeGare <excaliburhissheath@gmail.com>
2014-06-24 11:55:47 -0400

    Passage Selection in Editor

--- editor/ht_editor.js
+++ editor/ht_editor.js
@@ -6,4 +6,13 @@ var ht_editor = document.querySelector('#editor');
 
     editor.passages = [ new hypertext.Passage() ];
     editor.selectedPassage = editor.passages[0];
+
+    editor.newPassage = function (e, detail, sender) {
+        editor.passages.push(new hypertext.Passage());
+    };
+
+    editor.selectPassage = function (e, detail, sender) {
+        var index = sender.attributes.index.value;
+        editor.selectedPassage = editor.passages[index];
+    };
 }(hypertext, ht_editor));
