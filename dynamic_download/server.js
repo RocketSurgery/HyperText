0768dd77fd5197f12d4307f1cbf24363e359ddb6
David LeGare <excaliburhissheath@gmail.com>
2014-06-26 18:55:05 -0400

    Dynamic File Creation
    The files in the dynamic_download directory demonstrate using a nodejs server to create a downloadable file based on content written by the user in the webpage.

---
+++ dynamic_download/server.js
@@ -0,0 +1,61 @@
+/*global require, process, console*/
+
+var http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs"),
+    port = 8080,
+    webroot = '../';
+
+http.createServer(function (request, response) {
+    'use strict';
+
+    if (request.method === "GET") {
+        var uri = webroot + url.parse(request.url).pathname,
+            filename = path.join(process.cwd(), uri);
+
+        console.log(filename);
+
+        fs.exists(filename, function (exists) {
+            if (!exists) {
+                response.writeHead(404, {
+                    "Content-Type": "text/plain"
+                });
+                response.write("404 Not Found\n");
+                response.end();
+                return;
+            }
+
+            if (fs.statSync(filename).isDirectory()) {
+                filename += '/index.html';
+            }
+
+            fs.readFile(filename, "binary", function (err, file) {
+                if (err) {
+                    response.writeHead(500, {
+                        "Content-Type": "text/plain"
+                    });
+                    response.write(err + "\n");
+                    response.end();
+                    return;
+                }
+
+                response.writeHead(200);
+                response.write(file, "binary");
+                response.end();
+            });
+        });
+    } else if (request.method === "POST") {
+        console.log("request to POST made:");
+        request.on('data', function (data) {
+            response.writeHead(200, {
+                'Content-Type': 'text/plain',
+                'Content-Disposition': 'attachment; filename="balls.txt"'
+            });
+            response.write(data);
+            response.end();
+        });
+    }
+}).listen(port);
+
+console.log("Static file server running at\n  => http://localhost:" + port);
